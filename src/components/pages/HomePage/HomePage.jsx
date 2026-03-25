9ae7b983154ff47416717c25ce3775fd9f147f76
lin <lin.ferr@mail.ru>
2026-03-25 11:29:50 +0500

    first commit

---
+++ src/components/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import styles from './HomePage.module.css'
+import Header from '../../Header/Header'
+
+function HomePage() {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default HomePage
